9137c0dcd14fcb34d193788026386a84d39d994a
melissatvs <melissa.tv@hotmail.com>
2021-02-14 09:26:29 -0300

    feat: melhorando miniatura de compartilhamento

--- pages/_app.js
+++ pages/_app.js
@@ -41,14 +41,14 @@ export default function App({ Component, pageProps }) {
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://naturebaquiz.melissatvs.vercel.app/" />
         <meta property="og:title" content="Quiz Natureba" />
-        <meta property="og:description" content="" />
-        <meta property="og:image" content="https://i.imgur.com/f2EnIiU.jpg" />
+        <meta property="og:description" content="Quiz criado por Melissa durante a imersão Alura de Next.js" />
+        <meta property="og:image" content="https://i.imgur.com/57iFFrf.jpg" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://naturebaquiz.melissatvs.vercel.app/" />
         <meta property="twitter:title" content="Quiz Natureba" />
-        <meta property="twitter:description" content="" />
-        <meta property="twitter:image" content="https://i.imgur.com/f2EnIiU.jpg" />
+        <meta property="twitter:description" content="Quiz criado por Melissa durante a imersão Alura de Next.js" />
+        <meta property="twitter:image" content="https://i.imgur.com/57iFFrf.jpg" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;0,900;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
